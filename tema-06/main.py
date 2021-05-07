da34b88cfa704f2e08878e5cc349d3f587b60d01
Giovanna Gadelha <giovannagadelha1606@gmail.com>
2021-05-07 12:59:52 -0300

    Updates

--- tema-06/main.py
+++ tema-06/main.py
@@ -1,1 +1,6 @@
+from movies_analysis import top10_actors
+from tweets_analysis import select_tweets
 
+actorsList = top10_actors()
+
+select_tweets(actorsList)
--- tema-06/tweets_analysis.py
+++ tema-06/tweets_analysis.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import tweepy as tw
 
 
@@ -11,4 +12,41 @@ def authentication():
     auth = tw.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     return tw.API(auth)
+
+
+def select_tweets(top10_actors):
+    api = authentication()
+
+    tweets_dict = {}
+    tweets_dict = tweets_dict.fromkeys(['actor', 'created_at', 'id', 'id_str', 'text', 'truncated', 'entities',
+                                        'metadata', 'source', 'in_reply_to_status_id', 'in_reply_to_status_id_str',
+                                        'in_reply_to_user_id', 'in_reply_to_user_id_str', 'in_reply_to_screen_name',
+                                        'user', 'geo', 'coordinates', 'place', 'contributors', 'is_quote_status',
+                                        'retweet_count', 'favorite_count', 'favorited', 'retweeted'])
+
+    for actor in top10_actors:
+        search_query = actor + "-filter:retweets"
+        tweets = tw.Cursor(api.search, q=search_query).items(10)
+
+        for tweet in tweets:
+            for key in tweets_dict.keys():
+                try:
+                    if key == 'actor':
+                        twkey = actor
+                        tweets_dict[key].append(twkey)
+                    else:
+                        twkey = tweet._json
+                        tweets_dict[key].append(twkey)
+                except KeyError:
+                    twkey = ""
+                    if tweets_dict[key] is None:
+                        tweets_dict[key] = [twkey]
+                    else:
+                        tweets_dict[key].append[twkey]
+                except:
+                    tweets_dict[key] = [twkey]
+
+    tweets_dataframe = pd.DataFrame.from_dict(tweets_dict)
+
+    tweets_dataframe.to_csv("tweetsTop10Actors.csv", index=False)
 
