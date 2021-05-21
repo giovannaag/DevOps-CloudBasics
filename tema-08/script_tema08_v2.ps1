7ea3b94ef24cc21c786700b2272071caf29a3c1a
giovannaag <giovannagadelha1606@gmail.com>
2021-05-21 11:46:40 -0300

    Tema 08

---
+++ tema-08/script_tema08_v2.ps1
@@ -0,0 +1,5 @@
+Set-Location C:\Users\Giovanna\Desktop\devops-cloudbasics\tema-06
+Python main.py 
+
+Set-Location C:\Windows\System32
+aws s3 sync C:\Users\Giovanna\Desktop\devops-cloudbasics\tema-06\tweets s3://jt-dataeng-giovannagadelha/tema08/tweets/
